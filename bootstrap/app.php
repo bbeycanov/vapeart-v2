19cab4454be0c49c35149f99f543d97605e142d9
beycanbeycanov <bbeycanov@gmail.com>
2025-12-20 23:43:53 +0400

    feat: enhance error reporting for HTTP exceptions with detailed logging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Foundation\Application;
 use App\Http\Middleware\SetLocaleFromUrl;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,4 +19,19 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->report(function (Throwable $e) {
+            if ($e instanceof HttpExceptionInterface) {
+                Log::warning('HTTP EXCEPTION', [
+                    'status' => $e->getStatusCode(),
+                    'message' => $e->getMessage(),
+                    'url' => request()->fullUrl(),
+                    'method' => request()->method(),
+                    'ip' => request()->ip(),
+                    'user_id' => optional(auth()->user())->id,
+                    'origin' => request()->headers->get('origin'),
+                    'referer' => request()->headers->get('referer'),
+                    'cookie_present' => request()->headers->has('cookie'),
+                ]);
+            }
+        });
     })->create();
