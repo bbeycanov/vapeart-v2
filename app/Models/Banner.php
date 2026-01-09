6ac0c7195b3813bf1b6b40a986c80509ad85362b
beycanbeycanov <bbeycanov@gmail.com>
2026-01-10 00:03:42 +0400

    refactor: simplify mobile media URL retrieval in banner component

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -152,7 +152,7 @@ class Banner extends Model implements HasMedia, Sortable
         return [
             'desktop' => $media?->getUrl('desktop') ?: $media?->getUrl() ?: '',
             'tablet' => $tabletMedia?->getUrl('tablet') ?: $media?->getUrl() ?: '',
-            'mobile' => $mobileMedia?->getUrl('mobile') ?: $media?->getUrl('mobile') ?: $media?->getUrl() ?: '',
+            'mobile' => $mobileMedia?->getUrl('mobile') ?: $media?->getUrl() ?: '',
         ];
     }
 }
